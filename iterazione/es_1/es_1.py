fdc6f7608c5ce81982eeebf361ddcd170d52a9fc
Bergantin Nico <75247663+NicoBergantin19@users.noreply.github.com>
2022-01-20 12:52:58 +0100

    Esercizio 1

---
+++ iterazione/es_1/es_1.py
@@ -0,0 +1,15 @@
+#Dato N un numero intero positivo, generare e visualizzare il numero successivo.
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from pydoc import doc
+
+
+N = int(input("Inserisci un numero: "))
+if(N < 0):
+    N = input('Reinserisci il numero: ')
+    exit()
+else:
+    N += 1
+    print("Il numero modificato è: ", N)
