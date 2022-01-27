1141311761f62723c565564b3cd73a6dea5ee914
MorikawaKyota <75248450+MorikawaKyota@users.noreply.github.com>
2022-01-27 13:07:14 +0100

    Add files via upload

--- if e cicli/es_11/es11.py
+++ if e cicli/es_11/es11.py
@@ -4,7 +4,7 @@
 n1 = (int)(input("Inserire un numero positivo intero maggiore di zero.\n"))
 
 while n1 <= 0:
-    print("Errore, riprova\n")
+    print("Errore, riprova")
     n1 = (int)(input("Inserire un numero positivo intero maggiore di zero.\n"))
 
 riga = 0
@@ -12,7 +12,7 @@ while riga < n1:
     colonna = 0
     while colonna < n1:
         if  riga != 0 and n1-riga != 1 and colonna != 0 and n1-colonna != 1:
-            print(" ", end='')
+            print(" ", end='')#attenzione che la funzione print() aggiunge automaticamente una newline
         else:
             print("*", end='')
         colonna += 1
