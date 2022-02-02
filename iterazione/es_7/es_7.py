1475e501b44a356098c04bd6805b3f14ef79a11d
Samuelesiviero25 <93381552+Samuelesiviero25@users.noreply.github.com>
2022-02-02 09:50:42 +0100

    Add files via upload

---
+++ iterazione/es_7/es_7.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+n1=int(input("Inserisci un numero intero maggiore o uguale ad 1\n"))
+if(n1<1):
+    exit()
+else:
+    n2=n1-1
+    print ("Il numero precedente ad N è:",n2)
+
