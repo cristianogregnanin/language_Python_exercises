ac8b8d315420066473bba590e18ca9e680a50d67
MorikawaKyota <75248450+MorikawaKyota@users.noreply.github.com>
2022-02-03 14:41:44 -0800

    Add files via upload

--- if e cicli/es_9/es_9.py
+++ if e cicli/es_9/es_9.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-terni = [[0 for x in range(3)] for y in range(1)]
+terni = []
 indice = 0
 massimo = 0
 minimo = -1
-while True:
-    ternoInput = [[0 for x in range(3)] for y in range(1)]
-    ternoInput[0][0] = int(input("Inserire il primo numero del terno\n"))
-    ternoInput[0][1] = int(input("Inserire il secondo numero del terno\n"))
-    ternoInput[0][2] = int(input("Inserire il terzo numero del terno\n"))
 
-    for numero in range(len(ternoInput[0])):
-        if ternoInput[0][numero] < 0:
-            print("Errore, hai inserito un numero negativo")
-            #si calcolano il massimo ed il minimo
-            for x in range(len(terni)):
-                if terni[x][0] < terni[x][1] and terni[x][1] < terni[x][2]:
-                    if terni[x][2] > massimo:
-                        massimo = terni[x][2]
-                    if minimo == -1 or terni[x][0] < minimo:#si deve valorizzare il minimo la prima volta che i numeri del terno sono in ordine strettamente crescente
-                        minimo = terni[x][0]
-                indice += 1
-            #si visualizzano il massimo ed il minimo
-            print("Il minimo vale %d, il massimo vale %d" %(minimo, massimo))
-            break
-        else:
-            terni + ternoInput
-            
+while True:
+    ternoInput = [None] * 3
+    ternoInput[0] = int(input("Inserire il primo numero del terno\n"))
+    ternoInput[1] = int(input("Inserire il secondo numero del terno\n"))
+    ternoInput[2] = int(input("Inserire il terzo numero del terno\n"))
+
+    if ternoInput[0] < 0 or ternoInput[1] < 0 or ternoInput[2] < 0:
+        print("Errore, hai inserito un numero negativo")
+        break
+    else:
+        terni += ternoInput
+        print("Hai inserito i numeri del terno correttamente")
+
+#si controlla che sia stato inserito correttamente alemeno un terno
+if len(terni)  > 0:
+    #si calcolano il massimo ed il minimo
+    while indice < len(terni) -1:
+        terno = terni[indice : indice+3]
+        if terno[0] < terno[1] and terno[1] < terno[2]:
+            if terno[2] > massimo:
+                massimo = terno[2]
+            if minimo == -1 or terno[0] < minimo:#si deve valorizzare il minimo la prima volta che i numeri del terno sono in ordine strettamente crescente
+                minimo = terno[0]
+        indice += 3
+    #si visualizzano il massimo ed il minimo
+    print("Il minimo vale %d, il massimo vale %d" %(minimo, massimo))
+else:
+    print("Nessun terno inserito e' in ordine strettamente crescente")
