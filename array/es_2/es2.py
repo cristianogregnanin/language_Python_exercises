39813ddc5d3ede200977709c16b24553640c74dd
MorikawaKyota <75248450+MorikawaKyota@users.noreply.github.com>
2022-02-05 14:59:09 -0800

    Update es2.py

--- array/es_2/es2.py
+++ array/es_2/es2.py
@@ -1,7 +1,12 @@
-ar = []
-n = 0
-while n < 5:
-    x = raw_input('Inserisci il numero: ')
-    ar.append(x)
-    n = n+1
-print(ar[::-1])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+array = [None] * 4
+i = 0
+
+while i < len(array):
+    array[i] = int(input("Inserire un numero per popolare l'array\n"))
+    i +=1
+print("Adesso l'array e' popolato completamente")
+
+print(array[::1])
