913749d8d841ef1ed16627734b1fcac270772915
Hua Davide <hua.davide@iisviolamarchesini.edu.it>
2023-11-22 10:09:57 +0100

    Perfezione esercizio 'funzioni'

--- funzioni/es_1/es1.py
+++ funzioni/es_1/es1.py
@@ -3,11 +3,19 @@
 
 # Leggere in input da tastiera due numeri maggiori di 0 e farne la somma.
 
+
 def somma(n1, n2):
     return n1 + n2
 
 
-n1 = int(input("Inserire un primo numero: "))
-n2 = int(input("Inserire un secondo numero: "))
+while True:
+    n1 = int(input("Inserire un primo numero: "))
+    n2 = int(input("Inserire un secondo numero: "))
+
+    if n1 > 0 and n2 > 0:
+        break
+    else:
+        print("Devi inserire numero > di 0\n")
+
 
 print("%d + %d = %d" % (n1, n2, somma(n1, n2)))
