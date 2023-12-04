31d8784fa7c1c47aec5c4a80b193d33d22396655
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2023-12-04 20:47:38 +0100

    Add files via upload

---
+++ socket/es_5/es5Client.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+import pickle
+from persona import Persona  
+
+
+server_ip = sys.argv[1]
+port = int(sys.argv[2])
+input_file = sys.argv[3]
+output_file = sys.argv[4]
+
+    
+file = open("rubrica", 'r')
+rubrica = [line.split() for line in file]
+file.close()
+
+persone = [Persona(nome, cognome, telefono) for nome, cognome, telefono in rubrica]
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_ip, port))
+
+client_socket.send(pickle.dumps(persone))
+
+data = client_socket.recv(100000)
+persone_ordinate = pickle.loads(data)
+
+file = open("rubricaout", 'w')
+for persona in persone_ordinate:
+    file.write(f"{persona.nome} {persona.cognome} {persona.telefono}\n")
+file.close()
+
+print(f"Rubrica ordinata salvata in {rubricaout.txt}")
+
+    
+client_socket.close()
+
+
+
+
+ 
