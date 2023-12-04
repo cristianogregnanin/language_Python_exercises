dd01748d3a3217777a72785227d57c253ad99c13
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2023-12-04 20:59:14 +0100

    Create persona.py

---
+++ socket/es_5/persona.py
@@ -0,0 +1,5 @@
+class persona:
+    def __init__(self, nome, cognome, numero_di_telefono):
+        self.nome = nome
+        self.cognome = cognome
+        self.numero_di_telefono = numero_di_telefono
