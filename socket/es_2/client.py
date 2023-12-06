6a04b401aa99cafc714ba65f1e18781e607390f3
Alessandro <cesaro.alessandro@iisviolamarchesini.edu.it>
2023-12-06 11:25:01 +0100

    Sistemato file client.py, corretto controllo presenza file e lunghezza argomenti

--- socket/es_2/client.py
+++ socket/es_2/client.py
@@ -2,14 +2,16 @@ import socket
 import sys
 import os
 
-
-if len(sys.argv) < 5 or not os.path.exists(sys.argv[1]):
+if len(sys.argv) != 4:
     print("Errore negli argomenti <ip> <porta> <file>")
     exit()
-PORT = int(sys.argv[3])
+elif os.path.exists(sys.argv[3]) == False:
+    print("Il file non esiste")
+    exit()
+PORT = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((sys.argv[2], PORT))
-file = open(sys.argv[4], "r")
+s.connect((sys.argv[1], PORT))
+file = open(sys.argv[3], "r")
 text = file.read()
 s.send(text.encode())
 str_reverse = s.recv(1024).decode()
