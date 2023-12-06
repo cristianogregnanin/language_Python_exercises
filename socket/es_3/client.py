8898fc9dc1f743a2665be4454c748ebc46589a25
Hua Davide <hua.davide@iisviolamarchesini.edu.it>
2023-12-06 11:21:24 +0100

    Sistemato es_3 socket

--- socket/es_3/client.py
+++ socket/es_3/client.py
@@ -2,15 +2,17 @@ import socket
 import sys
 import os
 
-if len(sys.argv) < 6 or not os.path.exists(sys.argv[4]):
+if len(sys.argv) != 5 or not os.path.exists(sys.argv[3]):
     print("Errore negli argomenti <ip> <porta> <file> <carattere>")
     exit()
+
 PORT = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((sys.argv[1], PORT))
+
 file = open(sys.argv[3], "r")
 text = file.read()
-carattere = sys.argv[4]
+carattere = sys.argv[4][0]
 s.send(text.encode())
 s.recv(1024)
 s.send(carattere.encode())
