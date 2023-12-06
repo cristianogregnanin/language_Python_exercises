93ab9208e09d0e743d46c5e5e1d93c5b802441fb
zheng huichao <zheng.huichao@iisviolamarchesini.edu.it>
2023-12-06 11:23:49 +0100

    es4 corretto

--- socket/es_4/client.py
+++ socket/es_4/client.py
@@ -5,11 +5,11 @@ import sys
 if len(sys.argv) < 5:
     print("Errore negli argomenti <ip> <porta> <stringa1> <stringa2>")
     exit()
-PORT = int(sys.argv[4])
+PORT = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((sys.argv[3], PORT))
-string1 = sys.argv[1]
-string2 = sys.argv[2]
+s.connect((sys.argv[1], PORT))
+string1 = sys.argv[3]
+string2 = sys.argv[4]
 s.send(string1.encode())
 str_reverse = s.recv(1024).decode()
 print("Stringa 1 reverse ricevuta dal server: ", str_reverse)
