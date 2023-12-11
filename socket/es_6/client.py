1bd0b30dd30f45ffe219debb30163877bed0a232
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2023-12-11 14:42:12 +0100

    es 6

---
+++ socket/es_6/client.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+if len(sys.argv) < 3:
+    print("Errore negli argomenti <ip> <porta>")
+    print("Esempio: $ python3 client.py libero.it 80 ")
+    exit()
+
+argv = sys.argv
+
+HOST = argv[1]
+PORT = int(argv[2])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+http_request = """GET / HTTP/1.1
+Host: #{HOST}
+Connection: Keep-Alive\r\n\n"""
+
+http_request = "GET / HTTP/1.1\r\nHost:#{HOST}\r\n\r\n"
+
+s.send(http_request.encode())
+
+http_response = s.recv(4096).decode()
+
+print(http_response)
+s.close
---
+++ socket/es_6/client_libreria_request.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.get("https://iisviolamarchesini.edu.it")
+
+print(response.headers)
